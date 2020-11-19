4d6d4188d015558dded87a1ad66be8ca381a048c
levy <zlw5307@163.com>
2020-11-19 18:59:15 +0800

    db_wrapper

---
+++ src/server/rocksdb_wrapper.cpp
@@ -0,0 +1,166 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "rocksdb_wrapper.h"
+
+#include <dsn/utility/fail_point.h>
+
+#include "logging_utils.h"
+#include "pegasus_write_service.h"
+#include "pegasus_write_service_impl.h"
+
+namespace pegasus {
+namespace server {
+
+rocksdb_wrapper::rocksdb_wrapper(pegasus_server_impl *server) : replica_base(server) {}
+
+int rocksdb_wrapper::db_write_batch_put(int64_t decree,
+                                        dsn::string_view raw_key,
+                                        dsn::string_view value,
+                                        uint32_t expire_sec)
+{
+    return db_write_batch_put_ctx(db_write_context::empty(decree), raw_key, value, expire_sec);
+}
+
+int rocksdb_wrapper::db_write_batch_put_ctx(const db_write_context &ctx,
+                                            dsn::string_view raw_key,
+                                            dsn::string_view value,
+                                            uint32_t expire_sec)
+{
+    FAIL_POINT_INJECT_F("db_write_batch_put",
+                        [](dsn::string_view) -> int { return FAIL_DB_WRITE_BATCH_PUT; });
+
+    uint64_t new_timetag = ctx.remote_timetag;
+    if (!ctx.is_duplicated_write()) { // local write
+        new_timetag = generate_timetag(ctx.timestamp, get_cluster_id_if_exists(), false);
+    }
+
+    if (ctx.verify_timetag &&         // needs read-before-write
+        _pegasus_data_version >= 1 && // data version 0 doesn't support timetag.
+        !raw_key.empty()) {           // not an empty write
+
+        db_get_context get_ctx;
+        int err = db_get(raw_key, &get_ctx);
+        if (dsn_unlikely(err != 0)) {
+            return err;
+        }
+        // if record exists and is not expired.
+        if (get_ctx.found && !get_ctx.expired) {
+            uint64_t local_timetag =
+                pegasus_extract_timetag(_pegasus_data_version, get_ctx.raw_value);
+
+            if (local_timetag >= new_timetag) {
+                // ignore this stale update with lower timetag,
+                // and write an empty record instead
+                raw_key = value = dsn::string_view();
+            }
+        }
+    }
+
+    rocksdb::Slice skey = utils::to_rocksdb_slice(raw_key);
+    rocksdb::SliceParts skey_parts(&skey, 1);
+    rocksdb::SliceParts svalue = _value_generator.generate_value(
+        _pegasus_data_version, value, db_expire_ts(expire_sec), new_timetag);
+    rocksdb::Status s = _batch.Put(skey_parts, svalue);
+    if (dsn_unlikely(!s.ok())) {
+        ::dsn::blob hash_key, sort_key;
+        pegasus_restore_key(::dsn::blob(raw_key.data(), 0, raw_key.size()), hash_key, sort_key);
+        derror_rocksdb("WriteBatchPut",
+                       s.ToString(),
+                       "decree: {}, hash_key: {}, sort_key: {}, expire_ts: {}",
+                       ctx.decree,
+                       utils::c_escape_string(hash_key),
+                       utils::c_escape_string(sort_key),
+                       expire_sec);
+    }
+    return s.code();
+}
+
+int rocksdb_wrapper::db_write_batch_delete(int64_t decree, dsn::string_view raw_key)
+{
+    FAIL_POINT_INJECT_F("db_write_batch_delete",
+                        [](dsn::string_view) -> int { return FAIL_DB_WRITE_BATCH_DELETE; });
+
+    rocksdb::Status s = _batch.Delete(utils::to_rocksdb_slice(raw_key));
+    if (dsn_unlikely(!s.ok())) {
+        ::dsn::blob hash_key, sort_key;
+        pegasus_restore_key(::dsn::blob(raw_key.data(), 0, raw_key.size()), hash_key, sort_key);
+        derror_rocksdb("WriteBatchDelete",
+                       s.ToString(),
+                       "decree: {}, hash_key: {}, sort_key: {}",
+                       decree,
+                       utils::c_escape_string(hash_key),
+                       utils::c_escape_string(sort_key));
+    }
+    return s.code();
+}
+
+int rocksdb_wrapper::db_write(int64_t decree)
+{
+    dassert(_batch.Count() != 0, "");
+
+    FAIL_POINT_INJECT_F("db_write", [](dsn::string_view) -> int { return FAIL_DB_WRITE; });
+
+    rocksdb::Status status =
+        _batch.Put(_meta_cf, meta_store::LAST_FLUSHED_DECREE, std::to_string(decree));
+    if (dsn_unlikely(!status.ok())) {
+        derror_rocksdb("Write",
+                       status.ToString(),
+                       "put decree of meta cf into batch error, decree: {}",
+                       decree);
+        return status.code();
+    }
+
+    status = _db->Write(_wt_opts, &_batch);
+    if (dsn_unlikely(!status.ok())) {
+        derror_rocksdb("Write", status.ToString(), "write rocksdb error, decree: {}", decree);
+    }
+    return status.code();
+}
+
+int rocksdb_wrapper::db_get(dsn::string_view raw_key, /*out*/ db_get_context *ctx)
+{
+    FAIL_POINT_INJECT_F("db_get", [](dsn::string_view) -> int { return FAIL_DB_GET; });
+
+    rocksdb::Status s = _db->Get(_rd_opts, utils::to_rocksdb_slice(raw_key), &(ctx->raw_value));
+    if (dsn_likely(s.ok())) {
+        // success
+        ctx->found = true;
+        ctx->expire_ts = pegasus_extract_expire_ts(_pegasus_data_version, ctx->raw_value);
+        if (check_if_ts_expired(utils::epoch_now(), ctx->expire_ts)) {
+            ctx->expired = true;
+        }
+        return 0;
+    }
+    if (s.IsNotFound()) {
+        // NotFound is an acceptable error
+        ctx->found = false;
+        return 0;
+    }
+    ::dsn::blob hash_key, sort_key;
+    pegasus_restore_key(::dsn::blob(raw_key.data(), 0, raw_key.size()), hash_key, sort_key);
+    derror_rocksdb("Get",
+                   s.ToString(),
+                   "hash_key: {}, sort_key: {}",
+                   utils::c_escape_string(hash_key),
+                   utils::c_escape_string(sort_key));
+    return s.code();
+}
+} // namespace server
+} // namespace pegasus
---
+++ src/server/rocksdb_wrapper.h
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <dsn/utility/string_view.h>
+#include <dsn/dist/replication/replica_base.h>
+#include <base/pegasus_value_schema.h>
+
+namespace pegasus {
+namespace server {
+class pegasus_server_impl;
+class db_write_context;
+class db_get_context;
+
+class rocksdb_wrapper : public dsn::replication::replica_base
+{
+public:
+    explicit rocksdb_wrapper(pegasus_server_impl *server);
+
+    int db_write_batch_put(int64_t decree,
+                           dsn::string_view raw_key,
+                           dsn::string_view value,
+                           uint32_t expire_sec);
+
+    int db_write_batch_put_ctx(const db_write_context &ctx,
+                               dsn::string_view raw_key,
+                               dsn::string_view value,
+                               uint32_t expire_sec);
+
+    int db_write_batch_delete(int64_t decree, dsn::string_view raw_key);
+
+    int db_write(int64_t decree);
+
+    int db_get(dsn::string_view raw_key, /*out*/ db_get_context *ctx);
+
+private:
+    pegasus_value_generator _value_generator;
+};
+} // namespace server
+} // namespace pegasus
